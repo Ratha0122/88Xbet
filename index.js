7294e0d86066448863ba40d1540be74ba37661de
Ratha0122 <thoeurnratha1@gmail.com>
2024-06-29 11:23:52 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,125 @@
+function url(){
+    location.href = "https://www.hay88.one/?inviteCode=5910373"
+}
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    var navToggle = document.querySelector('#nav-toggle');
+    var navMenu = document.querySelector('.nav');
+
+    navToggle.addEventListener('click', function () {
+        navMenu.classList.toggle('show');
+    });
+});
+
+document.getElementsByClassName('header')[0].innerHTML = `
+     <div class="bg-[#fff] shadow-lg border-b border-black ">
+        <div class="max-w-6xl mx-auto px-4 py-5 ">
+            <div class="flex justify-between items-center gap-4 ">
+                <div class="flex  justify-center gap-8 items-center font-[500] text-[16px]">
+                    <a href="index.html" class="logo">
+                        <img src="./image/logo/8xbet_light-logo.png" alt="logo">
+                    </a>
+                    <div class="nav">
+                        <ul
+                            class="absolute *:list-none hidden md:relative md:flex flex-wrap gap-6 left-0 md:top-0 top-16 bg-slate-800 md:bg-transparent w-full md:w-auto md:text-black text-white text-center py-4 *:py-2">
+                            <li><a href="xo-so.html"
+                                    class="hover:text-[#007BFF] hover:border-b-2 hover:border-[#007BFF] ease-in-out duration-500">Xổ
+                                    Số</a></li>
+                            <li><a href="the-thao.html"
+                                    class="hover:text-[#007BFF] hover:border-b-2 hover:border-[#007BFF] ease-in-out duration-500">Thể
+                                    Thao</a></li>
+                            <li><a href="song-bac.html"
+                                    class="hover:text-[#007BFF] hover:border-b-2 hover:border-[#007BFF] ease-in-out duration-500">
+                                    Sòng bạc</a></li>
+                            <li><a href="88bet.html"
+                                    class="hover:text-[#007BFF] hover:border-b-2 hover:border-[#007BFF] ease-in-out duration-500">88xbet</a>
+                            </li>
+                        </ul>
+
+                    </div>
+                </div>
+
+                <div
+                    class="hidden btn md:flex justify-center items-center flex-wrap gap-4 *:bg-[#2cbd5c] text-white font-[500] *:py-1 *:px-4 *:rounded-md text-[17px]">
+                    <button onclick="url()" type="button" class="">Đăng ký</button>
+                    <button onclick="url()" type="button" class="">Đăng Nhập</button>
+                </div>
+                <div
+                id="nav-toggle" class="md:hidden menu text-3xl w-10 h-8 text-center rounded-md shadow-sm shadow-slate-950 bg-[#134074] text-white">
+                    <ion-icon  name="menu-outline"></ion-icon>
+                </div>
+            </div>
+            <div
+                class="md:hidden btn flex justify-center items-center  gap-4 *:bg-[#2cbd5c] text-white font-[500] pt-4 *:py-1 *:px-4 *:rounded-md text-[17px]">
+                <button onclick="url()" type="button" class="">Đăng ký</button>
+                <button onclick="url()" type="button" class="">Đăng Nhập</button>
+            </div>
+        </div>
+
+    </div>
+`
+
+document.getElementsByClassName('footer')[0].innerHTML = `
+    <footer class="">
+        <div class="max-w-6xl mx-auto pb-10 px-2">
+            <div class="grid grid-cols-1 md:grid-cols-2 justify-center gap-8">
+                <div class="txt *:pt-8">
+                    <h2>Giới Thiệu 88Xbet</h2>
+                    <p>Cá cược thể thao 88XBET, Xổ số, Bắn cá, Game bài, SLot game và nhiều trò chơi khác, Thưởng thức cá cược trực tuyến đỉnh cao và uy tín chỉ có tại 88XBet.</p>
+                    <img src="./image/index/partner-12.png" alt="">
+                </div>
+                <div class="txt *:pt-8">
+                    <h2>Nội dung tương tự</h2>
+                    <ul class="space-y-3">
+                        <li><a href="nha-cai.html" class="hover:text-[#11138f]">nhà cái 88xbet uy tin khong</a></li>
+                        <li><a href="rut-tien.html" class="hover:text-[#11138f]">rút tiền 88xbet</a></li>
+                        <li><a href="nap-tien.html" class="hover:text-[#11138f]">nạp tiền 88xbet</a></li>
+                        <li><a href="dang-ky.html" class="hover:text-[#11138f]"> đăng ký 88xbet </a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="text-center pt-2 pb-4">
+            <p>2024 Copyright © 88Xbet. All Rights Reserved.</p>
+        </div>
+    </footer>
+`
+
+document.getElementsByClassName('right-content')[0].innerHTML = `
+    <div class="shadow-lg shadow-[#3f373733]">
+                            <a href="cach-doc-keo-tai-xiu.html" class="" >
+                                <img src="./image/index/index-2.png" alt="">
+                                <div class="py-4">
+                                    <h3 class="py-3 px-2">Cách đọc kèo tài xỉu 1 1/4 chuẩn cho người mới</h3>
+                                    <p class="py-3 px-2">Tháng Năm 20, 2024 /// Không có bình luận</p>
+                                    <a href="" class=" text-[#2ab91d] py-3 px-2 font-bold hover:text-[#307929] duration-500">Read More »	</a><br>
+                                </div>
+                            </a>
+                        </div>
+
+                        <div class="shadow-lg shadow-[#3f373733]">
+                            <a href="cach-doc-keo-tai-xiu-2-5.html" class="" >
+                                <img src="./image/index/index-3.png" alt="">
+                                <div class="py-4">
+                                    <h3 class="py-3 px-2">
+                                        Cách đọc kèo tài xỉu 2.5 chuẩn xác nhất		</h3>
+                                    <p class="py-3 px-2">Tháng Năm 20, 2024 /// Không có bình luận</p>
+                                    <a href="" class=" text-[#2ab91d] py-3 px-2 font-bold hover:text-[#307929] duration-500">Read More »	</a><br>
+                                </div>
+                            </a>
+                        </div>
+
+
+                        <div class="shadow-lg shadow-[#3f373733]">
+                            <a href="tai-xiu.html" class="" >
+                                <img src="./image/index/index-4.png" alt="">
+                                <div class="py-4">
+                                    <h3 class="py-3 px-2">
+                                        Tài xỉu 2 1/4 – Kinh nghiệm bắt chuẩn như chuyên gia </h3>
+                                    <p class="py-3 px-2">Tháng Năm 20, 2024 /// Không có bình luận</p>
+                                    <a href="" class=" text-[#2ab91d] py-3 px-2 font-bold hover:text-[#307929] duration-500">Read More »	</a><br>
+                                </div>
+                            </a>
+                        </div>
+`
